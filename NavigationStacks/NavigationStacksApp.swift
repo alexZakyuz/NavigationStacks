cdb4cae05f2a89cee3e3224c6a100f6ba94ba37e
Alexandra Akyuz <bananalex2009@gmail.com>
2025-07-24 09:02:52 -0700

    Initial Commit

---
+++ NavigationStacks/NavigationStacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStacksApp.swift
+//  NavigationStacks
+//
+//  Created by Scholar on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
