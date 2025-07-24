2a5374b3817532ede9227cc88cd594197753bf6c
Alexandra Akyuz <bananalex2009@gmail.com>
2025-07-24 09:41:00 -0700

    added second text view and formatted

--- NavigationStacks/ContentView.swift
+++ NavigationStacks/ContentView.swift
@@ -9,15 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+        NavigationStack{
+            VStack{
+                Text("This is the root view 🌳")
+                    .font(.title)
+                    .foregroundColor(Color.green)
+                NavigationLink(destination: Text("You've arrived at the second view 🎉")
+                    .font(.title)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 20.0)) {
+                        Text("Click me!")
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.white)
+                            .background(Rectangle()
+                                .cornerRadius(5))
+                }//navigation link
+                
+            }//vstack
+        }//navigationstack
+    }//body
+}//struct
+
 
 #Preview {
     ContentView()
